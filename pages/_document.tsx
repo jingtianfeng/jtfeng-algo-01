25491e96fead20d759cba10172acad1ad46e5389
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-18 09:26:24 -0400

    FIX page _document: remove the 2 preconnect links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,15 +5,6 @@ const Document = () => {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com"
-            rel="preconnect"
-          />
-          <link
-            href="https://fonts.gstatic.com"
-            rel="preconnect"
-            crossOrigin="true"
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
