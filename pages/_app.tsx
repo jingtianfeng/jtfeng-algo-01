e747fe0881277337eafbf3dad9940748211e47d6
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-20 17:42:23 -0400

    FIX page _app: type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
