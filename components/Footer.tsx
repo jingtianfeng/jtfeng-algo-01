caa88c2e9c987e8a8245ad688d7416bc1c5d68bd
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-24 16:57:46 -0400

    FIX component Footer: .container padding to match sections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC<{}> = () => {
   return (
     <footer id="footer" 
             className="border-top">
-      <div className="container py-6">
+      <div className="container py-7">
         <div className="row">
           <div className="col-4">
             (sitemap)
