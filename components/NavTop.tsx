4f267add811510961343d2168989f524a62f8bdd
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-24 16:40:09 -0400

    ADD TO component NavTop: basic layout

--- components/NavTop.tsx
+++ components/NavTop.tsx
@@ -1,7 +1,19 @@
 const NavTop: React.FC<{}> = () => {
   return (
-    <nav>
-      <div className="container">NavTop</div>
+    <nav id="nav-top" 
+         className="border-bottom">
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-4 col-md-1">
+            <p>
+              DS/Algo in TypeScipt
+            </p>
+          </div>
+          <div className="col-4 col-md-3">
+            (nav)
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
