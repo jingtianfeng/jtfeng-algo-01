5547a022a17153c71f4ca5a2605ec3ba1e47b898
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-30 14:40:24 -0400

    UPDATE component NavTop: start working on nav

--- components/NavTop.tsx
+++ components/NavTop.tsx
@@ -4,14 +4,16 @@ const NavTop: React.FC<{}> = () => {
   return (
     <nav id="nav-top" className="border-bottom">
       <div className="container py-4">
-        <div className="row gy-2">
+        <div className="row gy-2 align-items-center justify-content-between">
           <div className="col-4 col-md-1">
             <Link href="/">
               <a>DS/Algo in TypeScript</a>
             </Link>
           </div>
-          <div className="col-4 col-md-3">
-            <div className="border">(nav)</div>
+          <div className="col-4 col-md-1 text-end">
+            <Link href="#footer__sitemap__leetcode">
+              <a>LeetCode</a>
+            </Link>
           </div>
         </div>
       </div>
