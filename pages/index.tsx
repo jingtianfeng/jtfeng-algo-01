ca37b3920cb4347d56c054197f47efd346865628
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-30 02:18:13 -0400

    UPDATE page index: remove style guide

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,118 +21,35 @@ const Home: NextPage<Props> = ({ code }) => {
       </Head>
       <header id="index-header">
         <div className="container py-7">
-          <div className="row gy-5">
-            <div className="col-4 col-lg-2 vstack gap-4">
-              <h1 className="border p-5">
-                The quick brown fox jumps over the lazy dog.
-              </h1>
-              <h2 className="border p-5">
-                The quick brown fox jumps over the lazy dog.
-              </h2>
-            </div>
-            <div className="col-4 col-lg-2 vstack gap-4">
-              <h3 className="border p-4">
-                The quick brown fox jumps over the lazy dog.
-              </h3>
-              <h4 className="border p-4">
-                The quick brown fox jumps over the lazy dog.
-              </h4>
-              <h5 className="border p-4">
-                The quick brown fox jumps over the lazy dog.
-              </h5>
-              <h6 className="border p-4">
-                The quick brown fox jumps over the lazy dog.
-              </h6>
-              <p className="border p-4">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </p>
-            </div>
+          <div class="prose">
+            <h1 className="text-capitalize">
+              20. Valid parentheses
+            </h1>
+            <p>
+              Given a string <code class="language-typescipt">s</code> 
+              containing just the characters 
+              <code class="language-typescipt">( )</code>
+              <code class="language-typescipt">{'{'} {'}'}</code>
+              <code class="language-typescipt">[ ]</code>
+              , determine if the input string is valid.
+            </p>
+            <p>
+              An input string is valid if:
+            </p>
+            <ol>
+              <li>Open brackets must be closed by the same type of brackets.</li>
+              <li>Open brackets must be closed in the correct order.</li>
+              <li>Every close bracket has a corresponding open bracket of the same type.</li>
+            </ol>
+            <pre>
+              <code
+                className="language-typescript"
+                dangerouslySetInnerHTML={{ __html: code }}
+              />
+            </pre>
           </div>
         </div>
       </header>
-      <section id="index-section1">
-        <div className="container py-7">
-          <div className="row gy-5">
-            <div className="col-4 col-lg-2 vstack gap-4">
-              <figure className="border p-4">
-                <blockquote className="blockquote">
-                  <p>A well-know quote, contained in a blockquote element.</p>
-                </blockquote>
-                <figcaption className="blockquote-footer">
-                  Someone famous in
-                  <cite title="Source title">Source title</cite>
-                </figcaption>
-              </figure>
-              <div className="border p-4">
-                <p>
-                  You can use the mark tag to <mark>highlight</mark> text.
-                </p>
-                <p>
-                  <strong>This line rendered as bold text.</strong>
-                </p>
-                <p>
-                  <em>This line rendered as italicized text.</em>
-                </p>
-                <p>
-                  <s>This line is meant to be treated as no longer accurate.</s>
-                </p>
-              </div>
-            </div>
-            <div className="col-4 col-lg-2 vstack gap-4">
-              <p className="lead border p-4">
-                This is a lead paragraph. It stands out from regular paragraphs.
-              </p>
-              <ul className="list-unstyled border p-4">
-                <li>This is a list.</li>
-                <li>It appears completely unstyled.</li>
-                <li>Structurally, it is still a list.</li>
-                <li>This style only applies to immediate child elements.</li>
-                <li>
-                  Nested lists:
-                  <ul>
-                    <li>are unaffected by this style</li>
-                    <li>will still show a bullet</li>
-                    <li>and have appropriate left margin</li>
-                  </ul>
-                </li>
-                <li>This may still come in handy in some situations.</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section id="index-section2">
-        <div className="container py-7">
-          <div className="row gy-5">
-            <div className="col-4 col-lg-2 vstack gap-4">
-              <div className="border p-4">
-                <dl className="row gy-2">
-                  <dt className="col-4 col-md-1">D.L.</dt>
-                  <dd className="col-4 col-md-3">
-                    A description list is perfect for defining terms.
-                  </dd>
-                  <dt className="col-4 col-md-1">D.T.</dt>
-                  <dd className="col-4 col-md-3">
-                    <p>Definition for the term.</p>
-                    <p>And some more placeholder definition text.</p>
-                  </dd>
-                </dl>
-              </div>
-            </div>
-            <div className="col-4 col-lg-2 vstack gap-4">
-              <div className="border p-4">
-                <pre>
-                  <code
-                    className="language-typescript"
-                    dangerouslySetInnerHTML={{ __html: code }}
-                  />
-                </pre>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
     </>
   );
 };
