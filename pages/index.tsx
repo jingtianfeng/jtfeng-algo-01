c9d126a677191b5cd6179d1d2a9eae56a9b85643
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-30 03:22:33 -0400

    FORMAT all

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,33 +22,34 @@ const Home: NextPage<Props> = ({ code }) => {
       <header id="leetcode-20-question">
         <div className="container py-7">
           <div className="prose">
-            <h1 className="text-capitalize">
-              Valid parentheses
-            </h1>
+            <h1 className="text-capitalize">Valid parentheses</h1>
             <p>
-              Given a string <code className="language-typescipt">s</code> 
-              containing just the characters 
+              Given a string <code className="language-typescipt">s</code>
+              containing just the characters
               <code className="language-typescipt">( )</code>
-              <code className="language-typescipt">{'{'} {'}'}</code>
-              <code className="language-typescipt">[ ]</code>
-              , determine if the input string is valid.
+              <code className="language-typescipt">
+                {"{"} {"}"}
+              </code>
+              <code className="language-typescipt">[ ]</code>, determine if the
+              input string is valid.
             </p>
-            <p>
-              An input string is valid if:
-            </p>
+            <p>An input string is valid if:</p>
             <ol>
-              <li>Open brackets must be closed by the same type of brackets.</li>
+              <li>
+                Open brackets must be closed by the same type of brackets.
+              </li>
               <li>Open brackets must be closed in the correct order.</li>
-              <li>Every close bracket has a corresponding open bracket of the same type.</li>
+              <li>
+                Every close bracket has a corresponding open bracket of the same
+                type.
+              </li>
             </ol>
           </div>
         </div>
       </header>
       <section id="leetcode-20-solution" className="border-top">
         <div className="container py-7">
-          <h2 className="mb-4">
-            Solution
-          </h2>
+          <h2 className="mb-4">Solution</h2>
           <pre>
             <code
               className="language-typescript"
@@ -59,9 +60,7 @@ const Home: NextPage<Props> = ({ code }) => {
       </section>
       <section id="leetcode-20-results" className="border-top">
         <div className="container py-7">
-          <h2>
-            Results
-          </h2>
+          <h2>Results</h2>
         </div>
       </section>
     </>
--- pages/leetcode/20-valid-parentheses.tsx
+++ pages/leetcode/20-valid-parentheses.tsx
@@ -12,42 +12,40 @@ const ValidParentheses: NextPage<Props> = ({ code }) => {
     <>
       <Head>
         <title>LeetCode 20: Valid Parentheses</title>
-        <meta
-          name="description"
-          content="LeetCode 20: Valid Parentheses"
-        />
+        <meta name="description" content="LeetCode 20: Valid Parentheses" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header id="leetcode-20-question">
         <div className="container py-7">
           <div className="prose">
-            <h1 className="text-capitalize">
-              Valid parentheses
-            </h1>
+            <h1 className="text-capitalize">Valid parentheses</h1>
             <p>
-              Given a string <code className="language-typescipt">s</code> 
-              containing just the characters 
+              Given a string <code className="language-typescipt">s</code>
+              containing just the characters
               <code className="language-typescipt">( )</code>
-              <code className="language-typescipt">{'{'} {'}'}</code>
-              <code className="language-typescipt">[ ]</code>
-              , determine if the input string is valid.
+              <code className="language-typescipt">
+                {"{"} {"}"}
+              </code>
+              <code className="language-typescipt">[ ]</code>, determine if the
+              input string is valid.
             </p>
-            <p>
-              An input string is valid if:
-            </p>
+            <p>An input string is valid if:</p>
             <ol>
-              <li>Open brackets must be closed by the same type of brackets.</li>
+              <li>
+                Open brackets must be closed by the same type of brackets.
+              </li>
               <li>Open brackets must be closed in the correct order.</li>
-              <li>Every close bracket has a corresponding open bracket of the same type.</li>
+              <li>
+                Every close bracket has a corresponding open bracket of the same
+                type.
+              </li>
             </ol>
           </div>
         </div>
       </header>
       <section id="leetcode-20-solution" className="border-top">
         <div className="container py-7">
-          <h2 className="mb-4">
-            Solution
-          </h2>
+          <h2 className="mb-4">Solution</h2>
           <pre>
             <code
               className="language-typescript"
@@ -58,9 +56,7 @@ const ValidParentheses: NextPage<Props> = ({ code }) => {
       </section>
       <section id="leetcode-20-results" className="border-top">
         <div className="container py-7">
-          <h2>
-            Results
-          </h2>
+          <h2>Results</h2>
         </div>
       </section>
     </>
