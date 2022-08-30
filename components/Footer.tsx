ade6e77d69bc1ace09243940882f60141c14d947
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-30 14:57:53 -0400

    UPDATE component Footer: start working on sitemap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link";
+
 const Footer: React.FC<{}> = () => {
   return (
     <footer id="footer" className="border-top">
       <div className="container py-7">
+        <h3 id="footer__sitemap" 
+            className="mb-4">
+          Sitemap
+        </h3>
         <div className="row">
           <div className="col-4">
-            <div className="border">(sitemap)</div>
+            <h4 id="footer__sitemap__leetcode" className="mb-2">
+              LeetCode
+            </h4>
+            <ul className="list-unstyled">
+              <li>
+                <Link href="/leetcode/20-valid-parentheses">
+                  <a>0020 - valid parentheses</a>
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
