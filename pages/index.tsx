4ae4975a6bee6320b1ac91fbaba6dbd97b58504c
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-30 14:25:14 -0400

    UPDATE page index: remove leetcode 20

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import isValid from "../solutions/leetcode/20-valid-parentheses";
+import Link from "next/link";
 
 interface Props {
   code: string;
@@ -21,48 +22,11 @@ const Home: NextPage<Props> = ({ code }) => {
       </Head>
       <header id="leetcode-20-question">
         <div className="container py-7">
-          <div className="prose">
-            <h1 className="text-capitalize">Valid parentheses</h1>
-            <p>
-              Given a string <code className="language-typescipt">s</code>
-              containing just the characters
-              <code className="language-typescipt">( )</code>
-              <code className="language-typescipt">
-                {"{"} {"}"}
-              </code>
-              <code className="language-typescipt">[ ]</code>, determine if the
-              input string is valid.
-            </p>
-            <p>An input string is valid if:</p>
-            <ol>
-              <li>
-                Open brackets must be closed by the same type of brackets.
-              </li>
-              <li>Open brackets must be closed in the correct order.</li>
-              <li>
-                Every close bracket has a corresponding open bracket of the same
-                type.
-              </li>
-            </ol>
-          </div>
+          <Link href="/leetcode/20-valid-parentheses">
+            <a>0020 - valid parentheses</a>
+          </Link>
         </div>
       </header>
-      <section id="leetcode-20-solution" className="border-top">
-        <div className="container py-7">
-          <h2 className="mb-4">Solution</h2>
-          <pre>
-            <code
-              className="language-typescript"
-              dangerouslySetInnerHTML={{ __html: code }}
-            />
-          </pre>
-        </div>
-      </section>
-      <section id="leetcode-20-results" className="border-top">
-        <div className="container py-7">
-          <h2>Results</h2>
-        </div>
-      </section>
     </>
   );
 };
