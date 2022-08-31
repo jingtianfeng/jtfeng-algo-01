74b3440a06a7e3376b0dd62abb018980f8844554
jingtianfeng <jingtianfeng@hotmail.com>
2022-08-31 11:05:59 -0400

    FIX solution leetcode 0020: typos and whitespaces

--- solutions/leetcode/0020-valid-parentheses.ts
+++ solutions/leetcode/0020-valid-parentheses.ts
@@ -7,14 +7,14 @@ const isValid = (s: string): boolean => {
   // then             return true
   // else if          s only has 1 bracket
   // then             return false
-  // else if          s starts with a closing bracket
+  // else if          s starts with a closed bracket
   // then             return false
-  // else if          s starts with NOT a starting bracket
+  // else if          s starts with a non-bracket character
   // then             return false
   // else             ...
   // ----------------------------------------------------------------------------
-  // OBSERVATIONS     at this point, s is guaranteed to start with a
-  //                  starting bracket
+  // OBSERVATIONS     at this point, s is guaranteed to start with an
+  //                  open bracket, and to have a 2nd character.
   // ----------------------------------------------------------------------------
   if (s.length === 0) {
     console.log("S IS EMPTY");
@@ -23,7 +23,7 @@ const isValid = (s: string): boolean => {
     console.log("S ONLY HAS 1 BRACKET");
     return false;
   } else if ([")", "}", "]"].includes(s.charAt(0))) {
-    console.log("S STARTS WITH A CLOSING BRACKET");
+    console.log("S STARTS WITH A CLOSED BRACKET");
     return false;
   } else if (["(", "{", "["].includes(s.charAt(0)) === false) {
     console.log("S STARTS WITH A NON-BRACKET CHARACTER");
@@ -35,26 +35,23 @@ const isValid = (s: string): boolean => {
     // ============================================================================
     // 3. Define isValid
     // ----------------------------------------------------------------------------
-    const arrBracketStarting = ["(", "{", "["];
-    const arrBracketClosing = [")", "}", "]"];
+    const arrBracketOpen = ["(", "{", "["];
+    const arrBracketClosed = [")", "}", "]"];
     let stkBracket = [s.charAt(0)];
     for (let i = 1; i < s.length; i++) {
       console.log("stkBracket: ", stkBracket);
       const char = s.charAt(i);
-      if (
-        arrBracketStarting.includes(char) === false &&
-        arrBracketClosing.includes(char) === false
-      ) {
-        console.log(`CHARACTER AT INDEX ${i} IS NON-BRACKET`);
+      if (!arrBracketOpen.includes(char) && !arrBracketClosed.includes(char)) {
+        console.log(`CHARACTER AT INDEX ${i} IS NOT A BRACKET`);
         return false;
-      } else if (arrBracketStarting.includes(char)) {
+      } else if (arrBracketOpen.includes(char)) {
         stkBracket.push(char);
       } else {
         if (
-          arrBracketClosing.indexOf(char) !==
-          arrBracketStarting.indexOf(stkBracket[stkBracket.length - 1])
+          arrBracketClosed.indexOf(char) !==
+          arrBracketOpen.indexOf(stkBracket[stkBracket.length - 1])
         ) {
-          console.log("CLOSING BRACKET DOES NOT MATCH STARTING BRACKET");
+          console.log("CLOSED BRACKET DOES NOT MATCH OPEN BRACKET");
           return false;
         } else {
           stkBracket.pop();
@@ -62,7 +59,7 @@ const isValid = (s: string): boolean => {
       }
     }
     if (stkBracket.length > 0) {
-      console.log("AFTER ITERATION STACK IS NOT EMPTY");
+      console.log("AFTER ITERATION, STACK IS NOT EMPTY");
       return false;
     } else {
       console.log("ALL BRACKETS MATCH");
