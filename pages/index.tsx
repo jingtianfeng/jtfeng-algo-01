e9711b88493e7142a57b9b5c20e52b40f10f20a2
jingtianfeng <jingtianfeng@hotmail.com>
2022-09-01 13:31:55 -0400

    ADD TO page index: status

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,9 +12,33 @@ const Index: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header id="index-question">
+      <header id="index-status">
         <div className="container py-7">
-          <h2 className="border">(section title)</h2>
+          <div className="row g-4">
+            <div className="col-4 col-md-1">
+              <h2>Status</h2>
+            </div>
+            <div className="col-4 col-md-3">
+              <div className="prose">
+                <p className="lead text-uppercase">
+                  LeetCode
+                </p>
+                <p>
+                  Hi fellow grinders! I'm currently working on the 
+                  <em>Top Interview Questions</em> list:
+                </p>
+                <ul>
+                  <li><s>0020 - Valid Parentheses</s></li>
+                  <li>0013 - Roman to Integer</li>
+                  <li>0014 - Longest Common Prefix</li>
+                  <li>0028 - Implement strStr()</li>
+                </ul>
+                <p>
+                  Links to my solutions are down in the footer section!
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
     </>
