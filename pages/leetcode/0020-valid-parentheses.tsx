b739b9e478b7f63ea5d6d514127e21dcbc5632f8
jingtianfeng <jingtianfeng@hotmail.com>
2022-09-06 10:17:42 -0400

    ADD TO page leetcode 0020: start <section> testing

--- pages/leetcode/0020-valid-parentheses.tsx
+++ pages/leetcode/0020-valid-parentheses.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Terminal from "../../components/Terminal";
 
 interface Props {
   code: string;
@@ -60,6 +61,18 @@ const ValidParentheses: NextPage<Props> = ({ code }) => {
                   dangerouslySetInnerHTML={{ __html: code }}
                 />
               </pre>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="leetcode-0020-testing" className="border-top">
+        <div className="container py-7">
+          <div className="row g-4">
+            <div className="col-4 col-md-1 d-none d-md-block">
+              <h2>Testing</h2>
+            </div>
+            <div className="col-4 col-md-3">
+              <Terminal/>
             </div>
           </div>
         </div>
