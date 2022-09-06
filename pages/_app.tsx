84578201874a644fda28b120fb44f0e02770bd73
jingtianfeng <jingtianfeng@hotmail.com>
2022-09-06 10:57:48 -0400

    ADD TO page _app: import xterm.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import "xterm/css/xterm.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
