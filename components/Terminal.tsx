92b67966e9aec4b6f1ef298c66dd0c8f974fcb5b
jingtianfeng <jingtianfeng@hotmail.com>
2022-09-09 13:58:33 -0400

    ADD TO component Terminal: start logic

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -12,6 +12,22 @@ const Terminal = () => {
       xTerm.loadAddon(addonFit);
       xTerm.open(document.getElementById("terminal") as HTMLDivElement);
       addonFit.fit();
+      const prefix = "Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ";
+      xTerm.write(prefix);
+      let lineCurr = "";
+      xTerm.onKey(({ key, domEvent }) => {
+        console.log("domEvent\t\t", domEvent);
+        if (domEvent.code === "Enter") {
+          const fnResult = new Function(`return ${lineCurr}`);
+          const strResult = fnResult().toString();
+          xTerm.write("\r\n" + strResult);
+          xTerm.write("\r\n" + prefix);
+          lineCurr = "";
+        } else {
+          lineCurr += key;
+          xTerm.write(key);
+        }
+      });
     };
     initTerminal();
   }, []);
