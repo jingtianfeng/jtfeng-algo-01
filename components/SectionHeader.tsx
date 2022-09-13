9bdd6cb9bdc32e9dc66abf9db3b27642cb255321
jingtianfeng <jingtianfeng@gmail.com>
2022-09-13 09:06:24 -0400

    ADD TO component SectionHeader: refactor from page index, page leetcode 0020

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,37 +1,13 @@
-const SectionHeader = () => {
+const SectionHeader = ({ heading, children }) => {
   return (
-    <header id="leetcode-0020-question">
+    <header id={"section-" + heading.toLowerCase()}>
       <div className="container py-7">
         <div className="row g-4">
           <div className="col-4 col-md-1 d-none d-md-block">
-            <h1>Question</h1>
+            <h1>{heading}</h1>
           </div>
           <div className="col-4 col-md-3">
-            <div className="prose">
-              <p className="lead text-uppercase">0020 - valid parentheses</p>
-              <p>
-                Given a string <code className="language-typescipt">s</code>
-                &nbsp;containing just the characters&nbsp;
-                <code className="language-typescipt">( )</code>&nbsp;
-                <code className="language-typescipt">
-                  {"{"} {"}"}
-                </code>
-                &nbsp;
-                <code className="language-typescipt">[ ]</code>, determine if
-                the input string is valid.
-              </p>
-              <p>An input string is valid if:</p>
-              <ol>
-                <li>
-                  Open brackets must be closed by the same type of brackets.
-                </li>
-                <li>Open brackets must be closed in the correct order.</li>
-                <li>
-                  Every closed bracket has a corresponding open bracket of the
-                  same type.
-                </li>
-              </ol>
-            </div>
+            <div className="prose">{children}</div>
           </div>
         </div>
       </div>
