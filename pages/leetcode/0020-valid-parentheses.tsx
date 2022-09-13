9c5791c609830cd6746faedfb913bdc9bdc6d48a
jingtianfeng <jingtianfeng@gmail.com>
2022-09-13 09:58:16 -0400

    REFACTOR page leetcode 0020: SectionSolution

--- pages/leetcode/0020-valid-parentheses.tsx
+++ pages/leetcode/0020-valid-parentheses.tsx
@@ -4,6 +4,7 @@ import Terminal from "../../components/Terminal";
 import { useEffect } from "react";
 import isValid from "../../solutions/leetcode/0020-valid-parentheses";
 import SectionHeader from "../../components/SectionHeader";
+import SectionSolution from "../../components/SectionSolution";
 
 interface Props {
   code: string;
@@ -47,23 +48,7 @@ const ValidParentheses: NextPage<Props> = ({ code }) => {
           </li>
         </ol>
       </SectionHeader>
-      <section id="leetcode-0020-solution" className="border-top">
-        <div className="container py-7">
-          <div className="row g-4">
-            <div className="col-4 col-md-1 d-none d-md-block">
-              <h2>Solution</h2>
-            </div>
-            <div className="col-4 col-md-3">
-              <pre>
-                <code
-                  className="language-typescript"
-                  dangerouslySetInnerHTML={{ __html: code }}
-                />
-              </pre>
-            </div>
-          </div>
-        </div>
-      </section>
+      <SectionSolution code={code} />
       <section id="leetcode-0020-testing" className="border-top">
         <div className="container py-7">
           <div className="row g-4">
