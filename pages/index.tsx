3ffc9a9feb0cc9425a626671c8309683a7318343
jingtianfeng <jingtianfeng@gmail.com>
2022-09-13 09:09:51 -0400

    REFACTOR page index: SectionHeader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Terminal from "../components/Terminal";
+import SectionHeader from "../components/SectionHeader";
 
 const Index: NextPage = () => {
   return (
@@ -13,33 +14,22 @@ const Index: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header id="index-status">
-        <div className="container py-7">
-          <div className="row g-4">
-            <div className="col-4 col-md-1 d-none d-md-block">
-              <h2>Status</h2>
-            </div>
-            <div className="col-4 col-md-3">
-              <div className="prose">
-                <p className="lead text-uppercase">LeetCode</p>
-                <p>
-                  Hi fellow grinders! I am currently working on the
-                  <em>Top Interview Questions</em> list:
-                </p>
-                <ul>
-                  <li>
-                    <s>0020 - Valid Parentheses</s>
-                  </li>
-                  <li>0013 - Roman to Integer</li>
-                  <li>0014 - Longest Common Prefix</li>
-                  <li>0028 - Implement strStr()</li>
-                </ul>
-                <p>Links to my solutions are in the footer section below!</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <SectionHeader heading="Status">
+        <p className="lead text-uppercase">LeetCode</p>
+        <p>
+          Hi fellow grinders! I am currently working on the
+          <em>Top Interview Questions</em> list:
+        </p>
+        <ul>
+          <li>
+            <s>0020 - Valid Parentheses</s>
+          </li>
+          <li>0013 - Roman to Integer</li>
+          <li>0014 - Longest Common Prefix</li>
+          <li>0028 - Implement strStr()</li>
+        </ul>
+        <p>Links to my solutions are in the footer section below!</p>
+      </SectionHeader>
       <section id="index-playground" className="border-top">
         <div className="container py-7">
           <div className="row g-4">
