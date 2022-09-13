2526e5f7f63efc0bfb49018f3a8ae9f6873c8322
jingtianfeng <jingtianfeng@gmail.com>
2022-09-13 08:33:34 -0400

    CREATE component SectionTesting

---
+++ components/SectionTesting.tsx
@@ -0,0 +1,18 @@
+const SectionTesting = () => {
+  return (
+    <section id="leetcode-0020-testing" className="border-top">
+      <div className="container py-7">
+        <div className="row g-4">
+          <div className="col-4 col-md-1 d-none d-md-block">
+            <h2>Testing</h2>
+          </div>
+          <div className="col-4 col-md-3">
+            <Terminal />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionTesting;
