73d69034c2243d11d092535a6509225568bd3f8e
jingtianfeng <jingtianfeng@gmail.com>
2022-09-13 09:08:55 -0400

    REFACTOR page leetcode 0020: SectionHeader

--- pages/leetcode/0020-valid-parentheses.tsx
+++ pages/leetcode/0020-valid-parentheses.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Terminal from "../../components/Terminal";
 import { useEffect } from "react";
 import isValid from "../../solutions/leetcode/0020-valid-parentheses";
+import SectionHeader from "../../components/SectionHeader";
 
 interface Props {
   code: string;
@@ -23,42 +24,29 @@ const ValidParentheses: NextPage<Props> = ({ code }) => {
       <Head>
         <title>LeetCode 0020: Valid Parentheses</title>
       </Head>
-      <header id="leetcode-0020-question">
-        <div className="container py-7">
-          <div className="row g-4">
-            <div className="col-4 col-md-1 d-none d-md-block">
-              <h1>Question</h1>
-            </div>
-            <div className="col-4 col-md-3">
-              <div className="prose">
-                <p className="lead text-uppercase">0020 - valid parentheses</p>
-                <p>
-                  Given a string <code className="language-typescipt">s</code>
-                  &nbsp;containing just the characters&nbsp;
-                  <code className="language-typescipt">( )</code>&nbsp;
-                  <code className="language-typescipt">
-                    {"{"} {"}"}
-                  </code>
-                  &nbsp;
-                  <code className="language-typescipt">[ ]</code>, determine if
-                  the input string is valid.
-                </p>
-                <p>An input string is valid if:</p>
-                <ol>
-                  <li>
-                    Open brackets must be closed by the same type of brackets.
-                  </li>
-                  <li>Open brackets must be closed in the correct order.</li>
-                  <li>
-                    Every closed bracket has a corresponding open bracket of the
-                    same type.
-                  </li>
-                </ol>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <SectionHeader heading="Question">
+        <p className="lead text-uppercase">0020 - valid parentheses</p>
+        <p>
+          Given a string <code className="language-typescipt">s</code>
+          &nbsp;containing just the characters&nbsp;
+          <code className="language-typescipt">( )</code>&nbsp;
+          <code className="language-typescipt">
+            {"{"} {"}"}
+          </code>
+          &nbsp;
+          <code className="language-typescipt">[ ]</code>, determine if the
+          input string is valid.
+        </p>
+        <p>An input string is valid if:</p>
+        <ol>
+          <li>Open brackets must be closed by the same type of brackets.</li>
+          <li>Open brackets must be closed in the correct order.</li>
+          <li>
+            Every closed bracket has a corresponding open bracket of the same
+            type.
+          </li>
+        </ol>
+      </SectionHeader>
       <section id="leetcode-0020-solution" className="border-top">
         <div className="container py-7">
           <div className="row g-4">
